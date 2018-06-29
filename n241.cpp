09cadd215682d80d96522038dcbc79d370259c37
OdearOgy <hovhan198@gmail.com>
2018-06-29 19:04:45 +0400

    Solved problem

---
+++ n241.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int main() {
+  int n, i, positive_count = 0;
+  float num[1000], sum = 0.0, average;
+
+  std::cout << "Enter the numbers of data: ";
+  std::cin >> n;
+
+  for(i = 0; i < n; i++) {
+    std::cout << i + 1 << ". Enter number: ";
+    std::cin >> num[i];
+    if (num[i] > 0) {
+      positive_count++;
+      sum += num[i];
+      average = sum / positive_count;
+    }
+  }
+  std::cout << "Average = " << average << std::endl;
+
+  return 0;
+}
