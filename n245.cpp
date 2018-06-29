df33514cc8a5d4f951d74da72e06dd99313770d1
OdearOgy <hovhan198@gmail.com>
2018-06-29 22:31:42 +0400

    Solve the problem

---
+++ n245.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int main() {
+  int n, i;
+  float num[1000], sum = 0.0;
+
+  std::cout << "Enter length of an array: ";
+  std::cin >> n;
+
+  for (i = 0; i < n; i++) {
+    std::cout << i + 1 << ". Enter number: ";
+    std::cin >> num[i];
+    if (i % 2 == 1) {
+      sum += num[i];
+    }
+  }
+
+  std::cout << "Sum = " << sum  << std::endl;
+
+  return 0;
+}
