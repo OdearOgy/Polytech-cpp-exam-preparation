a628bec7a6e0ac5cc57c1764e5d79c0886019b43
OdearOgy <hovhan198@gmail.com>
2018-06-30 04:11:50 +0400

    Solve the problem

---
+++ n252.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int main() {
+  int n, i, j, a, b, product = 1;
+  float num[1000];
+
+  std::cout << "Enter the numbers of data: ";
+  std::cin >> n;
+
+  for (i = 0; i < n; i++) {
+    std::cout << i + 1 << ". Enter number: ";
+    std::cin >> num[i];
+  }
+
+  std::cout << "Enter a number: ";
+  std::cin >> a;
+  std::cout << "Enter a number greater than previus one: ";
+  std::cin >> b;
+
+  for (j = a - 1; j < b - 1; j++) {
+    product *= num[j];
+  }
+
+  std::cout << "The value is: " << product << std::endl;
+
+  return 0;
+}
