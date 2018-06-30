e1a37d9e6ba9d6a70cce9369b27b5e6f6668bf58
OdearOgy <hovhan198@gmail.com>
2018-06-30 04:25:32 +0400

    Solve the problem

---
+++ n253.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int main() {
+  int n, i, j, a, b, number_count = 0;
+  float num[1000];
+
+  std::cout << "Enter the numbers of data: ";
+  std::cin >> n;
+
+  for (i = 0; i < n; i++) {
+    std::cout << i + 1 << ". Enter number: ";
+    std::cin >> num[i];
+  }
+
+  std::cout << "Enter a number: ";
+  std::cin >> a;
+  std::cout << "Enter a number greater than previus: ";
+  std::cin >> b;
+
+  for (j = a; j < b - 1; j++) {
+    number_count++;
+  }
+
+  std::cout << "The value is " << number_count << std::endl;
+
+  return 0;
+}
