f27a94c675c3423172738cc10e29e425540d19e7
OdearOgy <hovhan198@gmail.com>
2018-07-01 19:45:54 +0400

    Solve the problem

---
+++ n254.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int main() {
+  int n, i, k, sum = 0;
+  float num[1000];
+
+  std::cout << "Enter the numbers of data: ";
+  std::cin >> n;
+  std::cout << "Enter a positive number: ";
+  std::cin >> k;
+
+  for (i = 0; i < n; i++) {
+    std::cout << i + 1 << ". Enter number: ";
+    std::cin >> num[i];
+
+    if (abs(num[i]) < k) {
+      sum += pow(num[i], 3);
+    }
+  }
+
+  std::cout << "The value is " << sum << std::endl;
+}
